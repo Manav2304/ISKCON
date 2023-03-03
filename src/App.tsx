8b4c5b4882aa5584db50b9ff70ec39823af4970f
Neel0851 <neelp8866180120@gmail.com>
2023-03-03 10:24:19 +0530

    feat : create festival page

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,10 @@
 import React from "react";
 import "./App.css";
-
+import FestivalPage from "./Pages/About/Festival/index";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <FestivalPage/>
     </div>
   );
 }
---
+++ src/Pages/About/Festival/index.tsx
@@ -0,0 +1,22 @@
+import { festivals } from "./constant";
+import { FestivalContainer, FestivalImage, FestivalInfo } from "./style";
+
+const FestivalPage = () => {
+  return (
+    <><p>Festival </p>
+    <FestivalContainer>
+      {festivals.map((festival) => (
+        <div key={festival.name}>
+          <FestivalImage src={festival.image} />
+          <FestivalInfo>
+            <h2>{festival.name}</h2>
+            <p>{festival.date}</p>
+            <p>{festival.description}</p>
+          </FestivalInfo>
+        </div>
+      ))}
+    </FestivalContainer></>
+  );
+};
+
+export default FestivalPage;
