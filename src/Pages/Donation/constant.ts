41ab9879fb73ff916973d061d91135ce5583a1db
Neel0851 <neelp8866180120@gmail.com>
2023-03-09 13:58:06 +0530

    feat : add user information and contain in donation page.

--- src/Pages/Donation/constant.ts
+++ src/Pages/Donation/constant.ts
@@ -23,115 +23,115 @@ export const FEED_COWS = [
 
 export const ADOPT_A_COW = [
 {
-  id: 1,
+  id: 5,
     title: 'Adopt 1 Cow for 1 Month',
     amount: 2000,
 },
 {
-  id: 2,
+  id: 6,
     title: 'Adopt 1 Cow for 3 Month',
     amount: 6000,
 },
 {
-  id: 3,
+  id: 7,
     title: 'Adopt 1 Cow for 1 Year',
     amount: 24000,
 },
 {
-  id: 4,
+  id: 8,
     title: 'Adopt 1 Cow for Its Lifetime',
     amount: 240000,
 },
 {
-  id: 5,
+  id: 9,
     title: 'Adopt 1 Calf for Its Lifetime',
     amount: 360000,
 },
 ];
 export const ANNA_DAAN =[
   {
-    id: 1,
+    id: 10,
       title: 'Donate 20 Meals',
       amount: 1000,
   },
   {
-    id: 2,
+    id: 11,
       title: 'Donate 40 Meals',
       amount: 2000,
   },
   {
-    id: 3,
+    id: 12,
       title: 'Donate 100 Meals',
       amount: 5000,
   },
   {
-    id: 4,
+    id: 13,
       title: 'Donate 200 Meals',
       amount: 10000,
   },{
-    id: 5,
+    id: 14,
       title: 'Donate 400 Meals',
       amount: 20000,
   },
   {
-    id: 6,
+    id: 15,
       title: 'Donate 1000 Meals',
       amount: 50000,
   },
   {
-    id: 7,
+    id: 16,
       title: 'Donate 2000 Meals',
       amount: 100000,
   },
   {
-    id: 8,
+    id: 17,
       title: 'Donate 10000 Meals',
       amount: 500000,
   },
 ];
 export const VAISHNAV_BHOJAN =[
   {
-    id: 1,
+    id: 18,
       title: 'Feed 10 Vaishnava per day',
       amount: 2000,
   },
   {
-    id: 2,
+    id: 19,
       title: 'Feed 20 Vaishnava per day',
       amount: 5000,
   },
   {
-    id: 3,
+    id: 20,
       title: 'Feed 50 Vaishnava per day',
       amount: 10000,
   },
   {
-    id: 4,
+    id: 21,
       title: 'Feed 100 Vaishnava per day',
       amount: 20000,
   },
   {
-    id: 5,
+    id: 22,
       title: 'Feed All Vaishnava per day',
       amount: 25000,
   },
   {
-    id: 6,
+    id: 23,
       title: 'Feed All Vaishnava for two day',
       amount: 50000,
   },
   {
-    id: 7,
+    id: 24,
       title: 'Feed All Vaishnava for four day',
       amount: 100000,
   },
   {
-    id: 8,
+    id: 25,
       title: 'Feed All Vaishnava for eight day',
       amount: 200000,
   },
   {
-    id: 9,
+    id: 26,
       title: 'Feed All Vaishnava for 12 day',
       amount: 300000,
   },
