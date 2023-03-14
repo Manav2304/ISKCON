a7f603e4ecf04478ff7842c871ecd9973a5c0369
Manav patel <manavpatel232002@gmail.com>
2023-03-14 19:26:52 +0530

    fix:fix navbar

--- src/Components/Navbar/index.tsx
+++ src/Components/Navbar/index.tsx
@@ -4,15 +4,20 @@ import { NavStyle } from "./style";
 const NavigationBar = () => {
   return (
     <NavStyle>
-      <div className="container ">
-        <nav className="navbar fixed-top navbar-expand-lg bg-body-tertiary">
-          <div className="container-fluid">
-            <Link className="nav-link" to="Pages/Donation">
-              Donation
-            </Link>
-          </div>
-        </nav>
-      </div>
+      <nav className="navbar fixed-top navbar-expand-lg bg-dark">
+        <div className="container-fluid">
+          <Link className="nav-logo" to="/">
+            <img
+              src="https://iskconvrindavan.com/_next/image?url=%2Fimages%2Flogo-black.png&w=1920&q=75"
+              alt="Logo"
+            />
+          </Link>
+
+          <Link className="nav-link" to="/Pages/Donation">
+            Donation
+          </Link>
+        </div>
+      </nav>
     </NavStyle>
   );
 };
