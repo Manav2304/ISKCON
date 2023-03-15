613429d6079ec1a05dad86253af1ff559f42c74c
Neel0851 <neelp8866180120@gmail.com>
2023-03-15 10:05:47 +0530

    feat: add css ,form.tsx and update index.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import React from "react";
-import { Routes, Route, BrowserRouter } from "react-router-dom";
+import {
+  Routes,
+  Route,
+  BrowserRouter,
+} from "react-router-dom";
 
 import NavigationBar from "./Components/Navbar";
 import MyCarousel from "./Pages/Home";
 import Home from "./Pages/Home";
 import DonationPage from "./Pages/Donation";
+import DonationFormPage from "./Pages/Donation/form";
 
 export const App: React.FunctionComponent = () => {
   return (
@@ -14,7 +19,8 @@ export const App: React.FunctionComponent = () => {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/Pages/Home" element={<MyCarousel />} />
-          <Route path="/pages/Donation" element={<DonationPage />} />
+          <Route path="/pages/Donation/" element={<DonationPage />} />
+               <Route path="/donate" element={<DonationFormPage />} />
         </Routes>
       </BrowserRouter>
     </>
