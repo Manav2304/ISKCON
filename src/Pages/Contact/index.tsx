4f28b7543ee31c6923276c1cfa1b89b0bbe35013
Priyansh1825 <priyanshpatel1103@gmail.com>
2023-03-24 11:37:48 +0530

    feat: Add mobile querry

--- src/Pages/Contact/index.tsx
+++ src/Pages/Contact/index.tsx
@@ -1,9 +1,9 @@
 /* eslint-disable jsx-a11y/iframe-has-title */
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
-import { ADDRESS, MOBILE_NUMBER, EMAIL, GUEST_HOUSE_INFO } from './constant';
-import { Box, Container, GuestHouseContainer, GuestHouseInfo } from './style';
+import { ADDRESS, MOBILE_NUMBER, EMAIL, GUEST_HOUSE_INFO } from "./constant";
+import { Box, Container } from "./style";
 
 const MapContainer = styled.div`
   grid-column: 1 / span 2;
@@ -14,12 +14,12 @@ export const Contact: React.FC = () => {
     <Container>
       <MapContainer>
         <h4>Map</h4>
-        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3684.822512820993!2d72.9239412!3d22.548320499999996!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x395e4e755a0a0f43%3A0x2c838153538b28f0!2sIskcon%20Rd%2C%20Patel%20Society%2C%20Vallabh%20Vidyanagar%2C%20Anand%2C%20Gujarat%20388120!5e0!3m2!1sen!2sin!4v1679494883968!5m2!1sen!2sin"
+        <iframe
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3684.822512820993!2d72.9239412!3d22.548320499999996!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x395e4e755a0a0f43%3A0x2c838153538b28f0!2sIskcon%20Rd%2C%20Patel%20Society%2C%20Vallabh%20Vidyanagar%2C%20Anand%2C%20Gujarat%20388120!5e0!3m2!1sen!2sin!4v1679494883968!5m2!1sen!2sin"
           width="1150"
           height="450"
           loading="lazy"
-        >
-        </iframe>
+        ></iframe>
       </MapContainer>
 
       <Box>
@@ -39,9 +39,7 @@ export const Contact: React.FC = () => {
 
       <Box>
         <h4>Guest House Booking </h4>
-        <GuestHouseContainer>
-          <GuestHouseInfo>{GUEST_HOUSE_INFO}</GuestHouseInfo>
-        </GuestHouseContainer>
+        {GUEST_HOUSE_INFO}
       </Box>
     </Container>
   );
--- src/Pages/Contact/style.ts
+++ src/Pages/Contact/style.ts
@@ -2,11 +2,15 @@ import styled from "styled-components";
 
 export const Container = styled.div`
   display: grid;
-  grid-template-columns: 1fr 1fr;
+  grid-template-columns: 1fr;
   grid-gap: 20px;
   padding: 20px;
   max-width: 1200px;
   margin: 0 auto;
+
+  @media (min-width: 768px) {
+    grid-template-columns: 1fr 1fr;
+  }
 `;
 
 export const Box = styled.div`
@@ -18,7 +22,7 @@ export const Box = styled.div`
   flex-direction: column;
   align-items: center;
   text-align: center;
-  min-width: 200px;
+  width: 100%;
   max-width: 300px;
 
   h4 {
@@ -30,41 +34,12 @@ export const Box = styled.div`
   }
 
   @media (min-width: 768px) {
-    min-width: auto;
+    width: auto;
     max-width: none;
-    flex: 1;
-    margin-right: 20px;
-
-    &:last-child {
-      margin-right: 0;
-    }
-  }
-`;
-
-export const GuestHouseContainer = styled.div`
-  padding: 20px;
-  border: 1px solid #fffff0;
-  border-radius: 5px;
-  background-color: #fff2f2;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  text-align: center;
-  min-width: 200px;
-  max-width: 300px;
-
-  h4 {
-    margin-bottom: 10px;
   }
 
-  p {
-    margin-bottom: 0;
-  }
-
-  @media (min-width: 768px) {
-    min-width: auto;
-    max-width: none;
-    flex: 1;
+  @media (max-width: 767px) {
+    grid-column: 1 / span 1;
   }
 `;
 
@@ -74,7 +49,6 @@ export const GuestHouseInfo = styled.p`
 `;
 
 export const MapContainer = styled.div`
-  grid-column: 1 / span 4;
   margin-bottom: 20px;
 
   h4 {
@@ -85,5 +59,9 @@ export const MapContainer = styled.div`
     width: 100%;
     height: 400px;
     border: none;
+  }
+
+  @media (min-width: 768px) {
+    grid-column: 1 / span 2;
   }
 `;
