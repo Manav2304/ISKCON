c424b64cc295537ac5824557c741c6b45cf49077
Priyansh1825 <priyanshpatel1103@gmail.com>
2023-03-27 13:43:32 +0530

    feat: add media query

--- src/Pages/Contact/index.tsx
+++ src/Pages/Contact/index.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import styled from "styled-components";
-
 import { address, email, guestHouse, mobileNumber } from "./constant";
 import { Box, Container } from "./style";
 
