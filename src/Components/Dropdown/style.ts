cc10d164bf8b5ad14bc4495d65f2f58463898f56
Manav patel <manavpatel232002@gmail.com>
2023-03-27 13:42:35 +0530

    feat: Add style in Dropdown

--- src/Components/Dropdown/style.ts
+++ src/Components/Dropdown/style.ts
@@ -5,13 +5,20 @@ import styled from "styled-components";
 export const TransparentDropdown = styled(Dropdown)`
   border: none;
 
+  .dropdown-toggle {
+    color: #7e5959;
+    background-color: transparent;
+  }
+
   .dropdown-menu {
     background-color: #7e5959;
   }
+
   .dropdown-item {
     color: white;
   }
 `;
+
 export const StyledLink = styled(Link)`
   color: red;
 `;
