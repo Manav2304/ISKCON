057d3088d9512129ec90f73b4fd31fee6db63c03
Priyansh1825 <priyanshpatel1103@gmail.com>
2023-03-28 11:56:00 +0530

    feat: Set Titlebase images

--- src/Pages/Contact/index.tsx
+++ src/Pages/Contact/index.tsx
@@ -9,7 +9,7 @@ import {
   Image,
   ImageDiv,
 } from "./style";
-import TitleBase from "../../assets/images/Titlebase.png";
+import titleBaseUrl from "../../assets/images/Titlebase.png";
 
 export const Contact: React.FC = () => {
   return (
@@ -17,7 +17,7 @@ export const Contact: React.FC = () => {
       <MapContainer>
         <Heading>Contact Us</Heading>
         <ImageDiv>
-          <Image src={TitleBase} />
+          <Image src={titleBaseUrl} />
         </ImageDiv>
         <MapFrame
           title="Google Map"
