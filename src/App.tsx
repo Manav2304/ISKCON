ffdf2ba79dadddf1daa3deb3ae042dd8b9b9505d
Manavpatel2005 <patelmanav0205@gmail.com>
2023-03-29 16:52:27 +0530

    feat: Created footer.

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import NavigationBar from "./Components/Navbar";
 import Home from "./Pages/Home";
 import FestivalPage from "./Pages/Festival";
+import { Footer } from "./Components/Footer";
 
 export const App: React.FunctionComponent = () => {
   return (
---
+++ src/Components/Footer/constant.ts
@@ -0,0 +1,29 @@
+export const schedules = [
+    { time: "04:10AM", name: "Samadhi Arati" },
+    { time: "04:30AM", name: "Mangala Arati" },
+    { time: "07:15AM", name: "Shringar Arati" },
+    { time: "07:25AM", name: "Guru Puja" },
+    { time: "08:00AM", name: "Bhagavatam" },
+    { time: "08:30AM", name: "Pushpa Arati" },
+    { time: "12:00PM", name: "Raj Bhoga Arati" },
+    { time: "12:45PM", name: "Temple Closes" },
+    { time: "04:00PM", name: "Utthapan Arati" },
+    { time: "06:30PM", name: "Sandhaya Arati" },
+    { time: "08:00PM", name: "Shayan Arati" },
+    { time: "08:15PM", name: "Temple Closes" },
+  ];
+  
+  export const contactInfos = [
+    {
+      icon: "home",
+      text: "Mota Bazar,VV Nagar,Anand,Gujarat",
+    },
+    {
+      icon: "envelope",
+      text: "iskconvvnagar@yahoo.in",
+    },
+    {
+      icon: "phone",
+      text: "+ 01 234 567 88",
+    },
+  ];
