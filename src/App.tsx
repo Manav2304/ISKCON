081dde3bd2307b25a23770405e7b5b44b9cd7451
Sanskarpanchal27 <sanskarpanchal27@gmail.com>
2023-03-30 11:49:09 +0530

    feat: Remove Dropdown and navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,11 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-
-import NavigationBar from "./Components/Navbar";
 import Media from "./Pages/Media";
 
 export const App: React.FunctionComponent = () => {
   return (
     <>
       <Router>
-        <NavigationBar />
         <Routes>
           <Route caseSensitive path="/media" element={<Media />} />
         </Routes>
