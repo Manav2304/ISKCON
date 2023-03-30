47d3a042f07c2c63eb88dee95b755a91a21325c9
Manavpatel2005 <patelmanav0205@gmail.com>
2023-03-30 09:58:10 +0530

    feat: Update navbar.

--- src/Components/Navbar/index.tsx
+++ src/Components/Navbar/index.tsx
@@ -1,25 +1,27 @@
 import { NavStyle, StyledLink, ImgStyle } from "./style";
-import { socialMediaIcon } from "../Navbar/constant";
+import { DropDown } from "../Dropdown";
+import { donationInfo, mediaInfo, socialMediaIcon } from "../Navbar/constant";
 import { MDBIcon } from "mdb-react-ui-kit";
 import logoUrl from "../../assets/images/iskconLogo.png";
+import { servicesInfo, dropdownItems, festivalInfo } from "./constant";
 
 export const NavigationBar = () => {
   return (
     <NavStyle>
       <nav className="navbar">
         <div className="container">
-          <StyledLink to="#">Go Seva</StyledLink>
-          <StyledLink to="#">Festival</StyledLink>
-          <StyledLink to="#">Services</StyledLink>
+          <DropDown items={dropdownItems} toggleTitle="ISKCON" />
+          <DropDown items={festivalInfo} toggleTitle="Festival" />
+          <DropDown items={servicesInfo} toggleTitle="Services" />
           <StyledLink to="/">
             <ImgStyle src={logoUrl} alt="Logo" />
           </StyledLink>
+          <DropDown items={mediaInfo} toggleTitle="media" />
           <StyledLink to="#">Contact</StyledLink>
-          <StyledLink to="#">Media</StyledLink>
-          <StyledLink to="/donation">Donation</StyledLink>
+          <DropDown items={donationInfo} toggleTitle="Donate" />
           <div>
             {socialMediaIcon.map((item) => (
-              <a key={item.name} href={item.href} className="me-1 text-reset">
+              <a key={item.name} href={item.href}>
                 <MDBIcon fab icon={item.icon} />
               </a>
             ))}
--- src/Components/Navbar/style.ts
+++ src/Components/Navbar/style.ts
@@ -20,18 +20,23 @@ export const StyledLink = styled(Link)`
 
 export const ImgStyle = styled.img`
   height: 70px;
+
   @media (max-width: 767px) {
     height: 30px;
   }
+
   @media (min-width: 768px) and (max-width: 991px) {
     height: 50px;
   }
+
   @media (min-width: 992px) and (max-width: 1199px) {
     height: 60px;
   }
+
   @media (min-width: 1200px) and (max-width: 1399px) {
     height: 70px;
   }
+
   @media (min-width: 1400px) {
     height: 85px;
   }
