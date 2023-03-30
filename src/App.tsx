447f0b7b823c1c9ed4a272f9a04f8e7d3e702cf4
Manavpatel2005 <patelmanav0205@gmail.com>
2023-03-30 11:08:47 +0530

    feat: Add dropdown component.

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,9 @@ import { NavigationBar } from "./Components/Navbar/index";
 import Home from "./Pages/Home";
 import FestivalPage from "./Pages/Festival";
 import { Contact } from "./Pages/Contact";
+import "@fortawesome/fontawesome-free/css/all.min.css";
+import "@fortawesome/fontawesome-free/css/fontawesome.css";
+import "@fortawesome/fontawesome-free/css/brands.css";
 
 export const App: React.FunctionComponent = () => {
   return (
