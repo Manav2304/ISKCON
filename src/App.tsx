25868b8f49ad89e2b54df91206e2b36c6283609e
Manav patel <manavpatel232002@gmail.com>
2023-03-31 10:46:21 +0530

    feat: Added pages from the main branch

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { HistoryPage } from "./Pages/History";
 import { FounderPage } from "./Pages/Founder";
 import { PhilosophyPage } from "./Pages/Philosophy";
 import { GoalsPage } from "./Pages/Goals";
+import FestivalPage from "./Pages/Festival";
 
 export const App: React.FunctionComponent = () => {
   return (
@@ -12,6 +13,7 @@ export const App: React.FunctionComponent = () => {
       <BrowserRouter>
         <Routes>
           <Route caseSensitive path="/" element={<HomePage />} />
+          <Route caseSensitive path="/festival" element={<FestivalPage />} />
           <Route caseSensitive path="/history" element={<HistoryPage />} />
           <Route caseSensitive path="/founder" element={<FounderPage />} />
           <Route
