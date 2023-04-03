af8d3fb31ec2178a1d970e88e308e0cd7955a8b8
Manav patel <manavpatel232002@gmail.com>
2023-04-03 09:56:43 +0530

    feat: Remove dropdown files

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,6 @@
 import React from "react";
 import { Routes, Route, BrowserRouter } from "react-router-dom";
 import { HomePage } from "./Pages/Home";
-import { HistoryPage } from "./Pages/History";
-import { FounderPage } from "./Pages/Founder";
-import { PhilosophyPage } from "./Pages/Philosophy";
-import { GoalsPage } from "./Pages/Goals";
 import { FestivalPage } from "./Pages/Festival";
 import { NavigationBar } from "./Components/Navbar";
 import { Contact } from "./Pages/Contact";
@@ -17,14 +13,6 @@ export const App: React.FunctionComponent = () => {
         <Routes>
           <Route caseSensitive path="/" element={<HomePage />} />
           <Route caseSensitive path="/festival" element={<FestivalPage />} />
-          <Route caseSensitive path="/history" element={<HistoryPage />} />
-          <Route caseSensitive path="/founder" element={<FounderPage />} />
-          <Route
-            caseSensitive
-            path="/philosophy"
-            element={<PhilosophyPage />}
-          />
-          <Route caseSensitive path="/goals" element={<GoalsPage />} />
           <Route caseSensitive path="/contact" element={<Contact />} />
         </Routes>
       </BrowserRouter>
