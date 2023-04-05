bd1ad619e08dfaea2701d3c376d02ac5cf9cef34
PATEL NEEL MAHESHKUMAR <124124229+Neel0851@users.noreply.github.com>
2023-04-05 10:47:08 +0530

    feat: Add routes file for defining route path constant (#36)
    * feat: Create routes.tsx file for routing configuration.
    
    * feat: Changes routes.tsx to routes.tsx.
    
    * feat: Update routes.ts file.
    
    * feat: Remove json-routes from package.json.
    
    ---------
    
    Co-authored-by: Priyansh1825 <priyanshpatel1103@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,9 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import NavigationBar from "./Components/Navbar";
 import Home from "./Pages/Home";
-import FestivalPage from "./Pages/Festival";
+import { FestivalPage } from "./Pages/Festival";
 import { Contact } from "./Pages/Contact";
+import { routes } from "./routes";
 
 export const App: React.FunctionComponent = () => {
   return (
@@ -11,9 +12,13 @@ export const App: React.FunctionComponent = () => {
       <Router>
         <NavigationBar />
         <Routes>
-          <Route caseSensitive path="/" element={<Home />} />
-          <Route caseSensitive path="/festival" element={<FestivalPage />} />
-          <Route caseSensitive path="/contact" element={<Contact />} />
+          <Route caseSensitive path={routes.home} element={<Home />} />
+          <Route
+            caseSensitive
+            path={routes.festival}
+            element={<FestivalPage />}
+          />
+          <Route caseSensitive path={routes.contact} element={<Contact />} />
         </Routes>
       </Router>
     </>
---
+++ src/routes.ts
@@ -0,0 +1,5 @@
+export const routes = {
+  home: "/",
+  festival: "/festival",
+  contact: "/contact",
+};
