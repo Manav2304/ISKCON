e20c3fdd073270904cc0848efd4e2108720ed14b
Neel139 <np32978@gmail.com>
2023-04-07 12:13:45 +0530

    feat: updated carousel

--- src/Pages/Visitus/constant.ts
+++ src/Pages/Visitus/constant.ts
@@ -6,14 +6,38 @@ import goushala4 from "../../assets/images/goushala4.jpg";
 import goushala5 from "../../assets/images/goushala5.jpg";
 import goushala6 from "../../assets/images/goushala6.jpg";
 
-export const dakorImage = [dakor];
-export const goushalaImage = [
-  goushala1,
-  goushala2,
-  goushala3,
-  goushala4,
-  goushala5,
-  goushala6,
+export const dakorImages = [
+  {
+    id: 1,
+    image: dakor,
+  },
+];
+
+export const goushalImages = [
+  {
+    id: 1,
+    image: goushala1,
+  },
+  {
+    id: 2,
+    image: goushala2,
+  },
+  {
+    id: 3,
+    image: goushala3,
+  },
+  {
+    id: 4,
+    image: goushala4,
+  },
+  {
+    id: 5,
+    image: goushala5,
+  },
+  {
+    id: 6,
+    image: goushala6,
+  },
 ];
 
 export const dakorInfo = [
