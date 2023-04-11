d54a57a3fde495a4f3e3e53ccf94e439f81b4708
Manav patel <manavpatel232002@gmail.com>
2023-04-11 10:06:53 +0530

    feat:Update navabr bg

--- src/Components/Navbar/style.ts
+++ src/Components/Navbar/style.ts
@@ -1,5 +1,5 @@
 import styled from "styled-components";
-import title from "../../assets/images/bg.png";
+import title from "../../assets/images/navbar-bg.png";
 import { Link } from "react-router-dom";
 
 export const NavStyle = styled.div`
