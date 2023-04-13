40da14eac52704d337aeb6d2466a951f35faacbb
Manav patel <manavpatel232002@gmail.com>
2023-04-13 16:44:03 +0530

    feat:Update navbar

--- src/Components/Navbar/index.tsx
+++ src/Components/Navbar/index.tsx
@@ -13,22 +13,41 @@ import { dropdownHomeInfo } from "./constant";
 import { routes } from "../../routes";
 
 export const NavigationBar = () => {
+  const handleLinkClick = () => {
+    window.scrollTo(0, 0);
+  };
+
   return (
     <NavStyle className="navbar fixed-top">
       <div className="container">
-        <PageDropdown items={dropdownHomeInfo} toggleTitle="I" />
+        <PageDropdown items={dropdownHomeInfo} toggleTitle="Iskcon" />
         <PageDropdown items={dropdownServicesInfo} toggleTitle="Services" />
-        <StyledLink to={routes.visitUs}>Visit Us</StyledLink>
-        <StyledLink to={routes.media}>Media</StyledLink>
-        <StyledLink to={routes.home}>
+        <StyledLink to={routes.visitUs} onClick={handleLinkClick}>
+          Visit Us
+        </StyledLink>
+        <StyledLink to={routes.media} onClick={handleLinkClick}>
+          Media
+        </StyledLink>
+        <StyledLink to={routes.home} onClick={handleLinkClick}>
           <ImgStyle src={logoUrl} alt="Logo" />
         </StyledLink>
-        <StyledLink to={routes.donation}>Donate</StyledLink>
-        <StyledLink to={routes.festival}>Festival</StyledLink>
-        <StyledLink to={routes.contact}>Contact</StyledLink>
+        <StyledLink to={routes.donation} onClick={handleLinkClick}>
+          Donate
+        </StyledLink>
+        <StyledLink to={routes.festival} onClick={handleLinkClick}>
+          Festival
+        </StyledLink>
+        <StyledLink to={routes.contact} onClick={handleLinkClick}>
+          Contact
+        </StyledLink>
         <SocialMediaIconsStyle>
           {socialMediaIcon.map((item) => (
-            <IconsStyle key={item.name} href={item.href} target="_blank">
+            <IconsStyle
+              key={item.name}
+              href={item.href}
+              target="_blank"
+              onClick={handleLinkClick}
+            >
               <MDBIcon fab icon={item.icon} />
             </IconsStyle>
           ))}
