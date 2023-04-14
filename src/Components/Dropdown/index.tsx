da87afacb9934421354a12e9fd42434a37e7d794
Manav patel <manavpatel232002@gmail.com>
2023-04-14 10:13:48 +0530

    feat:Update dropdown

--- src/Components/Dropdown/index.tsx
+++ src/Components/Dropdown/index.tsx
@@ -25,6 +25,7 @@ export const PageDropdown: React.FC<DropdownProps> = ({
   const [isHovered, setIsHovered] = useState(false);
 
   const handleItemClick = (path: string) => {
+    window.scrollTo(0, 0);
     navigate(path);
   };
 
