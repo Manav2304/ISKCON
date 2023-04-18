ec3d84f416a55d19cba52442979351c23995827e
Priyansh1825 <priyanshpatel1103@gmail.com>
2023-04-18 13:33:06 +0530

    feat: Update Sidebar

--- src/Components/Sidebar/constant.ts
+++ src/Components/Sidebar/constant.ts
@@ -1,23 +1,19 @@
 import { routes } from "../../routes";
 
 export const dropdownHomeInfo = [
-    { name: "History", url: "#" },
-    { name: "Founder", url: "#" },
-    { name: "Philosophy", url: "#" },
-    { name: "Goals", url: "#" },
-  ];
-  
+  { name: "History", url: "#" },
+  { name: "Founder", url: "#" },
+  { name: "Philosophy", url: "#" },
+  { name: "Goals", url: "#" },
+];
+
 export const dropdownServicesInfo = [
-    { name: "Life Time Membership", url: routes.lifeTimeMembership },
-    { name: "Kirtan At Iskcon", url: routes.kirtanAtIskcon },
-    { name: "Iskcon Youth Forum", url: routes.iskconYouthForum },
-    { name: "Guest House", url: routes.guestHouse },
-    { name: "Goushala", url: routes.goushala },
-    { name: "Devotee Kitchen", url: routes.devoteeKitchen },
-    { name: "Deity Worship", url: routes.deityWorship },
-    { name: "Book Distribution", url: routes.bookDistribution },
-  ];
-
-  
-
-  
+  { name: "Life Time Membership", url: routes.lifeTimeMembership },
+  { name: "Kirtan At Iskcon", url: routes.kirtanAtIskcon },
+  { name: "Iskcon Youth Forum", url: routes.iskconYouthForum },
+  { name: "Guest House", url: routes.guestHouse },
+  { name: "Goushala", url: routes.gaushala },
+  { name: "Devotee Kitchen", url: routes.devoteeKitchen },
+  { name: "Deity Worship", url: routes.deityWorship },
+  { name: "Book Distribution", url: routes.bookDistribution },
+];
