04210833184114903b19772c2181f708a3ec0656
Manavpatel2005 <patelmanav0205@gmail.com>
2023-04-22 12:46:17 +0530

    feat: Updated festival page.

--- src/Pages/Festival/style.ts
+++ src/Pages/Festival/style.ts
@@ -23,7 +23,6 @@ export const FestivalImage = styled.img`
   height: auto;
   object-fit: cover;
   border: 3px solid #963;
-  box-shadow: 0px 0px 5px 0px rgba(0, 0, 0, 0.2);
 
   @media (min-width: 768px) {
     width: 350px;
