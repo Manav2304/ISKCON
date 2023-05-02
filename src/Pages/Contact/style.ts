46e51bc14356d204776cefc64f530de0ceee0fd7
Priyansh1825 <124125728+Priyansh1825@users.noreply.github.com>
2023-05-02 10:54:56 +0530

    feat: Update contact & footer. (#96)
    * feat: Update contact & footer.
    
    * feat: Use map function in Contact.
    
    * feat: delete Unwanted code.

--- src/Pages/Contact/style.ts
+++ src/Pages/Contact/style.ts
@@ -4,11 +4,20 @@ export const Container = styled.div`
   margin: 5px;
 `;
 
+export const ContactLink = styled.a`
+  color: #fff;
+  text-decoration: none;
+
+  &:hover {
+    color: #ff007f;
+  }
+`;
+
 export const Box = styled.div`
   padding: 20px;
-  border: 1px solid #fb3cca;
+  border: 3px solid #fff;
   border-radius: 5px;
-  background-color: #ffa9a9;
+  background-color: #963;
   flex-direction: column;
   align-items: center;
   text-align: center;
@@ -35,9 +44,9 @@ export const Box = styled.div`
 
 export const GuestBox = styled.div`
   padding: 20px;
-  border: 1px solid #fb3cca;
+  border: 3px solid #fff;
   border-radius: 5px;
-  background-color: #ffa9a9;
+  background-color: #963;
   flex-direction: column;
   align-items: center;
   text-align: center;
@@ -73,7 +82,6 @@ export const MapFrame = styled.iframe`
 
   @media only screen and (max-width: 768px) {
     font-size: 1.5rem;
-    margin-top: 20px;
     margin-bottom: 50px;
     padding-left: 40px;
     padding-right: 40px;
@@ -81,7 +89,6 @@ export const MapFrame = styled.iframe`
 
   @media only screen and (min-width: 769px) and (max-width: 1199px) {
     font-size: 2rem;
-    margin-top: 50px;
     margin-bottom: 50px;
     padding-left: 50px;
     padding-right: 50px;
@@ -89,7 +96,6 @@ export const MapFrame = styled.iframe`
 
   @media only screen and (min-width: 1200px) {
     font-size: 3rem;
-    margin-top: 50px;
     margin-bottom: 50px;
     padding-left: 50px;
     padding-right: 50px;
@@ -99,7 +105,7 @@ export const MapFrame = styled.iframe`
 export const BoxStyle = styled.div`
   padding-left: 50px;
   padding-right: 50px;
-  margin-bottom: 10px;
+  margin-bottom: 50px;
   display: grid;
   grid-template-columns: repeat(2, 2fr);
   grid-gap: 20px;
@@ -115,11 +121,13 @@ export const BoxStyle = styled.div`
 `;
 
 export const BoxHeading = styled.h2`
-  margin-bottom: 10px;
+  margin-bottom: 5px;
   font-family: "Times New Roman", Times, serif;
+  color: #fff;
 `;
 
 export const BoxDetail = styled.p`
   font-family: "Times New Roman", Times, serif;
-  font-size: 16px;
+  font-size: 18px;
+  color: #fff;
 `;
