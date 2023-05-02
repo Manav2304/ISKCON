4f28d3ab57ab66034d3ed27be7a66465c4010a8b
Sanskarpanchal27 <sanskarpanchal27@gmail.com>
2023-05-02 23:33:52 +0530

    feat: create separate folder

---
+++ src/Pages/DonationPages/AnnaDaan/constant.ts
@@ -0,0 +1,7 @@
+export const annaDaan = [
+  " Anna Daan is Maha Daan. Lord Krishna is extremely pleased to bless a devotee who renders even a little service to Him.",
+
+  "In Mahabharata, Yudhishthira Maharaj enquired:'What gift will reward in this world and in the next?' Bhismadev answered:'There has never been, nor will there ever be, any gift equal to the gift of food.-Anusasana Parva 13.63.3-6",
+
+  "Please grab the opportunity to get the blessings of Lord Krishna by rendering this important service of Anna Daan Seva.",
+];
