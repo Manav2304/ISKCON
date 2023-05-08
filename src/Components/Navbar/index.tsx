b9dd812dc34cd33ef00c622f9694fe39ae199628
Manav patel <manavpatel232002@gmail.com>
2023-05-08 16:01:21 +0530

    feat: Update style.

--- src/Components/Navbar/index.tsx
+++ src/Components/Navbar/index.tsx
@@ -40,6 +40,7 @@ export const NavigationBar = () => {
           <StyledLink to={routes.donation}>Donate</StyledLink>
           <StyledLink to={routes.festival}>Festival</StyledLink>
           <StyledLink to={routes.contact}>Contact</StyledLink>
+          <StyledLink to={routes.contact}>Blogs</StyledLink>
         </div>
       </NavStyle>
     </>
