bd91c0b961be089d4e1cd08d040d5a9b28ff6150
Priyansh1825 <priyanshpatel1103@gmail.com>
2023-06-02 21:02:47 +0530

    feat: Add govinda's food service .

---
+++ src/Pages/Services/GovindaFood/constant.ts
@@ -0,0 +1,15 @@
+import govindaFood1 from "../../../assets/images/govinda-food1.png";
+import govindaFood2 from "../../../assets/images/govinda-food2.png";
+
+export const govindaFoodImages = [
+  {
+    id: 1,
+    image: govindaFood1,
+  },
+  {
+    id: 2,
+    image: govindaFood2,
+  },
+];
+
+export const govindaFood = [" Coming Soon "];
